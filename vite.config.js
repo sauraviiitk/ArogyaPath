a9303f6655763c08e402d5f9e785c29b50658770
Saurav Kumar <sauraviiitk18@gmail.com>
2025-03-12 14:26:57 +0530

    Fixed Vercel 404 issue with rewrites

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-})
+  server: {
+    historyApiFallback: true, // Ensures routing works in dev mode
+  },
+});
